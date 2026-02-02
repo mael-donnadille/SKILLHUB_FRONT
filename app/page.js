4307859332efeda6a84ee78ed8a636961bbff210
seiiiishinsan <gabinhalloss@gmail.com>
2026-02-02 14:48:05 +0100

    feat: setup components (navbar, footer)

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
     return (
-        <div>Accueil</div>
+        <div>
+            <Navbar />
+            Accueil
+        </div>
     );
 }
---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className={"w-screen h-20 bg-red-500"}>
+            <p>SkillHub</p>
+        </div>
+    )
+}
